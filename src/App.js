3a6bf9cfa18587f575168d4b843eff68eb1f081b
Evelyne Namwoyo <evelyne@laboremus.no>
2019-10-23 15:45:41 +0200

    Add css to new quote button

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ class App extends React.Component {
     super(props);
     this.onGetNewQuote = this.onGetNewQuote.bind(this);
     this.state = {
-      quote: {}
+      quote: {},
+      myColors: [
+                  '#800000', '#dab0a4', '#de3c59','#FF00FF', '#003366',
+                  '#808000', '#5e7c95', '#de3c59', '#572578','#009688',
+                  '#816c53', '#006400'
+                 ],
+      theColor: '',
+
     }
   }
 
@@ -25,11 +32,32 @@ class App extends React.Component {
     })
   }
 
+  backgroundStyle() {
+    return {backgroundColor:this.state.theColor}
+  }
+
+  componentDidMount() {
+    return this.onGetNewQuote();
+  }
+
+  componentDidUpdate(prevprops, prevState) {
+    const randomColor = Math.floor(Math.random() * this.state.myColors.length);
+    if(prevState.quote.content !== this.state.quote.content) {
+        this.setState(() => ({theColor:this.state.myColors[randomColor]}))
+        console.log(this.state.myColors[randomColor])
+    }
+  }
+
+  componentWillUnmount() {
+    console.log('will unamount')
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{backgroundColor:this.state.theColor}}>
         <Quotes quote = {this.state.quote}
           onGetNewQuote = {this.onGetNewQuote}
+          theColor = {this.state.theColor}
         />
       </div>
     )
@@ -39,10 +67,20 @@ class App extends React.Component {
 
 
   const Quotes =(props) => {
+    const styles = {
+      backgroundStyle: {
+        backgroundColor:props.theColor
+      },
+        colorStyle: {
+          color: props.theColor
+      }
+  };
+
+  const {backgroundStyle, colorStyle} = styles;
     return (
       <article id="quote-box" className="container">
         <div className="card">
-          <div className="card-body card-info">
+          <div className="card-body card-info" style={colorStyle}>
             <q className="card-text" id="text">
               {props.quote.content}
             </q>
@@ -56,6 +94,7 @@ class App extends React.Component {
             <button 
               onClick={props.onGetNewQuote}
               id="new-quote"
+              style={backgroundStyle}
             >
               New quote
             </button>
